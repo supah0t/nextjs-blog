feb5cd55f4420f09de3e65ff55f2c49dd7f69062
supah0t <odysseasmastorakis@hotmail.com>
2024-08-20 11:07:27 +0300

    test

---
+++ pages/dashboard/page.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export function Dashboard () {
+
+  const [data, setData] = useState();
+
+  const fetchData = async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    const stuff = await response.json();
+    setData(stuff.name);
+  }
+
+  return ( 
+    <div>
+      <h1> Dashboard </h1>
+      <button onClick={fetchData}>Fetch</button>
+      {data && <h3>{data}</h3>}
+    </div> 
+  );
+}
